1c9ad092f95e4cd86ea067a6ea4e2008fd942e00
Marina Sazonova <mst121315@gmail.com>
2025-04-20 23:06:30 +0200

    Add validation and api

---
+++ src/components/api.js
@@ -0,0 +1,74 @@
+const config = {
+  baseUrl: "https://nomoreparties.co/v1/wff-cohort-36",
+  headers: {
+    authorization: "85ed03d0-3030-4aec-8d2c-4308658bc7d8",
+    "Content-Type": "application/json",
+  },
+};
+
+function processResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+}
+
+export const getInitialCards = () => {
+  return fetch(`${config.baseUrl}/cards`, {
+    headers: config.headers,
+  }).then((res) => processResponse(res));
+};
+
+export const getProfile = () => {
+  return fetch(`${config.baseUrl}/users/me`, {
+    method: "GET",
+    headers: config.headers,
+  }).then((res) => processResponse(res));
+};
+
+export const updateProfile = (name, about) => {
+  return fetch(`${config.baseUrl}/users/me`, {
+    method: "PATCH",
+    headers: config.headers,
+    body: JSON.stringify({
+      name: name,
+      about: about,
+    }),
+  }).then((res) => processResponse(res));
+};
+
+export const addNewCard = (name, link) => {
+  return fetch(`${config.baseUrl}/cards`, {
+    method: "POST",
+    headers: config.headers,
+    body: JSON.stringify({
+      name: name,
+      link: link,
+    }),
+  }).then((res) => processResponse(res));
+};
+
+export const processLike = (isLike, cardId) => {
+  const method = isLike ? "PUT" : "DELETE";
+  return fetch(`${config.baseUrl}/cards/likes/${cardId}`, {
+    method: method,
+    headers: config.headers,
+  }).then((res) => processResponse(res));
+};
+
+export const processDelete = (cardId) => {
+  return fetch(`${config.baseUrl}/cards/${cardId}`, {
+    method: "DELETE",
+    headers: config.headers,
+  }).then((res) => processResponse(res));
+};
+
+export const updateAvatar = (avatar) => {
+  return fetch(`${config.baseUrl}/users/me/avatar`, {
+    method: "PATCH",
+    headers: config.headers,
+    body: JSON.stringify({
+      avatar: avatar,
+    }),
+  }).then((res) => processResponse(res));
+};
--- src/components/card.js
+++ src/components/card.js
@@ -1,35 +1,60 @@
-// @todo: Функция создания карточки
+function createCard(
+  element,
+  cardTemplate,
+  zoom,
+  myId,
+  processLike,
+  processDelete
+) {
+  const cardElement = cardTemplate.querySelector(".places__item").cloneNode(true);
+  const imageElement = cardElement.querySelector(".card__image");
+  const likeCountElement = cardElement.querySelector(".like-count");
 
-function createCard(element, cardTemplate, zoom) {
-    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
-    const imageElement = cardElement.querySelector('.card__image');
-  
-    imageElement.src = element.link;
-    imageElement.alt = element.name;
-  
-    const cardTitle = cardElement.querySelector('.card__title');
-    cardTitle.textContent = element.name;
-    const deleteButton = cardElement.querySelector('.card__delete-button');
-    deleteButton.addEventListener('click', () => deleteCard(cardElement));
-  
-    const likeButton = cardElement.querySelector('.card__like-button');    // лайк
-    likeButton.addEventListener('click', () => handleLike(likeButton));
-  
-    imageElement.addEventListener('click', () => zoom(element));    // zoom
-  
-    return cardElement;
-  }
-  
-  // @todo: Функция удаления карточки
-  
-  function deleteCard(cardElement) {
-    cardElement.remove();
-  };
-  
-  // лайк
-  
-  function handleLike (likeButton) {
-    likeButton.classList.toggle('card__like-button_is-active');
+  likeCountElement.textContent = element.likes.length;
+  imageElement.src = element.link;
+  imageElement.alt = element.name;
+  const cardId = element._id;
+
+  const cardTitle = cardElement.querySelector(".card__title");
+  cardTitle.textContent = element.name;
+
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  if (element.owner._id == myId) {
+    deleteButton.addEventListener("click", () =>
+      deleteCard(cardElement, deleteButton, processDelete)
+    );
+    deleteButton.dataset.cartId = cardId;
+  } else {
+    deleteButton.remove();
   }
 
-  export { createCard };
+  const likeButton = cardElement.querySelector(".card__like-button"); // лайк
+  likeButton.dataset.cartId = cardId;
+  likeButton.addEventListener("click", () =>
+    handleLike(likeButton, processLike, likeCountElement)
+  );
+  if (element.likes.some((user) => user._id === myId)) {
+    likeButton.classList.add("card__like-button_is-active");
+  }
+
+  imageElement.addEventListener("click", () => zoom(element)); // zoom
+
+  return cardElement;
+}
+
+function deleteCard(cardElement, deleteButton, processDelete) {
+  cardElement.remove();
+  processDelete(deleteButton.dataset.cartId);
+}
+
+function handleLike(likeButton, processLike, likeCountElement) {
+  likeButton.classList.toggle("card__like-button_is-active");
+  processLike(
+    likeButton.classList.contains("card__like-button_is-active"),
+    likeButton.dataset.cartId
+  ).then((element) => {
+    likeCountElement.textContent = element.likes.length;
+  });
+}
+
+export { createCard };
